b75ad2a8f80f7f2e7395f60a9d5152c5811f57bd
Nicklas Boserup <nickeopti@gmail.com>
2022-02-20 22:06:43 +0100

    Implement quantile and probabilistic filters/samplers

--- selection.py
+++ selection.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod
 from dataclasses import dataclass
-from typing import Any, Collection, List, Sequence, Tuple
+from typing import List, Tuple
 
 import numpy as np
 import torch
@@ -57,6 +57,44 @@ class NoFilterer(Filterer):
         ]
 
 
+class ThresholdFilterer(Filterer):
+    def __init__(self, threshold: float = 0.5, normalised: bool = True) -> None:
+        self.threshold = threshold
+        self.normalised = normalised
+
+    def filter(self, values: List[List[Regions]]) -> List[List[ContrastiveRegions]]:
+        normalisation_factor = values[0][0][0].size ** 2 if self.normalised else 1
+        return [
+            [
+                ContrastiveRegions(
+                    [region for region in channel if region.attention > self.threshold * normalisation_factor],
+                    [region for region in channel if region.attention < self.threshold * normalisation_factor]
+                )
+                for channel in image
+            ]
+            for image in values
+        ]
+
+
+class QuantileFilterer(ThresholdFilterer):
+    def __init__(self, threshold: float = 0.5, normalised: bool = True, quantile: float = 0.9) -> None:
+        super().__init__(threshold, normalised)
+        self.quantile = quantile
+
+    def preprocess(self, values: torch.Tensor) -> torch.Tensor:
+        b, c, *_ = values.shape
+        quantiles = [
+            torch.quantile(image.flatten(start_dim=-2), q=self.quantile, dim=-1)
+            for image in values
+        ]
+
+        result = values.clone()
+        for i in range(b):
+            for j in range(c):
+                result[i, j] = (values[i, j] >= quantiles[i][j]).type(values.type())
+        return result
+
+
 class SortedFilterer(Filterer):
     def __init__(self, n_positives_filter: int = 50) -> None:
         super().__init__()
@@ -81,27 +119,26 @@ class SortedFilterer(Filterer):
 
 class UniformSampler(Sampler):
     def sample(self, values: List[List[ContrastiveRegions]]) -> Tuple[List[List[Regions]], List[List[Regions]]]:
-        # res = [[None] * len(values)]
-        # for image_index, image in enumerate(values):
-        #     for channel_index, channel in enumerate(image):
-        #         positive_indices = np.random.choice(self.n_positives, replace=False)
-        #         negative_indices = np.random.choice(self.n_negatives, replace=False)
-
-        #         positives = [channel.positives[i] for i in positive_indices]
-        #         negatives = [channel.negatives[i] for i in negative_indices]
-
-        #         res[image_index][channel_index] = ContrastiveRegions(positives, negatives)
-
         positives = [
             [
-                [channel.positives[i] for i in np.random.choice(len(channel.positives), self.n_positives, replace=False)]
+                [
+                    channel.positives[i] for i in (
+                        np.random.choice(len(channel.positives), self.n_positives, replace=True)
+                        if len(channel.positives) > 0 else []
+                    )
+                ]
                 for channel in image
             ]
             for image in values
         ]
         negatives = [
             [
-                [channel.negatives[i] for i in np.random.choice(len(channel.negatives), self.n_negatives, replace=False)]
+                [
+                    channel.negatives[i] for i in (
+                        np.random.choice(len(channel.negatives), self.n_negatives, replace=True)
+                        if len(channel.negatives) else []
+                    )
+                ]
                 for channel in image
             ]
             for image in values
@@ -110,113 +147,49 @@ class UniformSampler(Sampler):
         return positives, negatives
 
 
-class RegionSelector:
-    @abstractmethod
-    def select(self, attended_image) -> Sequence[Collection[Region]]:
-        pass
-
-
-class AttentionBasedRegionSelector(RegionSelector):
-    def __init__(self, size: int, stride: int, device=None) -> None:
-        self.size = size
-        self.stride = stride
-
-        self.f = nn.Conv2d(
-            in_channels=1, out_channels=1, kernel_size=self.size, stride=self.stride, bias=False
-        )
-        self.f.weight = nn.Parameter(
-            torch.ones((1, 1, self.size, self.size), dtype=torch.float, device=device),
-            requires_grad=False,
-        )
-
-    def transform(self, attentions, where):
-        *_, channel, row, col = where
-        return [
-            Region(x * self.stride, y * self.stride, c, self.size, attentions[0, c, x, y] if attentions is not None else 0)
-            for c, x, y in zip(channel.tolist(), row.tolist(), col.tolist())  # implicit cast to floats, rather than tensors containing floats
+class ProbabilisticSampler(Sampler):
+    def sample(self, values: List[List[ContrastiveRegions]]) -> Tuple[List[List[Regions]], List[List[Regions]]]:
+        # Currently assumes that positives and negatives in each ContrastiveRegions are identical
+        activations = [
+            [
+                np.array([region.attention for region in channel.positives])
+                for channel in image
+            ]
+            for image in values
         ]
-
-
-class ContrastiveRegionsQuantileSelector(AttentionBasedRegionSelector):
-    def __init__(self, size: int, stride: int, quantile: float = 0.9, device=None) -> None:
-        super().__init__(size, stride, device)
-        self.quantile = quantile
-
-
-class SingleChannelQuantileSelector(ContrastiveRegionsQuantileSelector):
-    def __init__(self, size: int = 50, stride: int = 5, quantile: float = 0.9, device=None, *vargs, **kwargs) -> None:
-        super().__init__(size, stride, quantile, device)
-
-    def select(self, attended_image):
-        threshold = torch.quantile(attended_image, 0.9)
-        activated = (attended_image > threshold).type(torch.float)
-        slide_sum = self.f(activated)
-
-        positives = self.transform(slide_sum, torch.where(slide_sum > self.size**2 / 2))
-        negatives = self.transform(slide_sum, torch.where(slide_sum < self.size**2 / 2))
-        return positives, negatives
-
-
-class MultiChannelQuantileSelector(ContrastiveRegionsQuantileSelector):
-    def __init__(self, size: int = 50, stride: int = 5, quantile: float = 0.9, device=None) -> None:
-        super().__init__(size, stride, quantile, device)
-
-    def select(self, attended_image):
-        attentions = torch.stack([
-            (self.f((channel > torch.quantile(channel, self.quantile)).type(torch.float).unsqueeze(0).unsqueeze(0))).squeeze()
-            for channel in attended_image[0]
-        ]).unsqueeze(0)  # assuming batch size of 1
-        l = self.transform(attentions, torch.where(attentions > self.size ** 2 / 2))
-        return [
-            [region for region in l if region.channel == i]
-            for i in range(len(attended_image[0]))
+        sums = [
+            [
+                channel.sum()
+                for channel in image
+            ]
+            for image in activations
         ]
-
-
-class UniformRandomSelector(RegionSelector):
-    def __init__(self, size: int = 50, n_positives: int = 5, n_negatives: int = 32, device=None) -> None:
-        super().__init__()
-        self.size = size
-        self.n_positives = n_positives
-        self.n_negatives = n_negatives
-    
-    def select(self, attended_image):
-        *_, width, height = attended_image.shape
-        positives_rows = torch.randint(width - self.size, (self.n_positives,))
-        positives_cols = torch.randint(height - self.size, (self.n_positives,))
-        negatives_rows = torch.randint(width - self.size, (self.n_negatives,))
-        negatives_cols = torch.randint(height - self.size, (self.n_negatives,))
-        
+        positive_probabilities = [
+            [
+                channel_activations / channel_sum
+                for channel_activations, channel_sum in zip(image_activations, image_sums)
+            ]
+            for image_activations, image_sums in zip(activations, sums)
+        ]
+        negative_probabilities = [
+            [
+                (1 - channel) / (len(channel) - channel.sum())
+                for channel in image
+            ]
+            for image in positive_probabilities
+        ]
         positives = [
-            Region(row, col, 0, self.size, 0)
-            for row, col in zip(positives_rows, positives_cols)
+            [
+                [channel.positives[i] for i in np.random.choice(len(channel.positives), self.n_positives, replace=False, p=channel_probabilities)]
+                for channel, channel_probabilities in zip(image, image_probabilities)
+            ]
+            for image, image_probabilities in zip(values, positive_probabilities)
         ]
         negatives = [
-            Region(row, col, 0, self.size, 0)
-            for row, col in zip(negatives_rows, negatives_cols)
+            [
+                [channel.negatives[i] for i in np.random.choice(len(channel.negatives), self.n_negatives, replace=False, p=channel_probabilities)]
+                for channel, channel_probabilities in zip(image, image_probabilities)
+            ]
+            for image, image_probabilities in zip(values, negative_probabilities)
         ]
         return positives, negatives
-
-
-class SingleChannelSortedAttentionSelector(AttentionBasedRegionSelector):
-    def __init__(self, size: int = 50, stride: int = 20, n_positives: int = 30, device=None) -> None:
-        super().__init__(size, stride, device)
-        self.n_positives = n_positives
-
-    def select(self, attended_image) -> Sequence[Collection[Region]]:
-        attentions = self.f(attended_image)
-        regions = self.transform(attentions, torch.where(torch.ones_like(attentions)))
-        sorted_regions = list(sorted(regions, key=lambda region: region.attention, reverse=True))
-        return sorted_regions[:self.n_positives], sorted_regions[self.n_positives:]
-
-
-class MultiChannelSortedAttentionSelector(AttentionBasedRegionSelector):
-    def __init__(self, size: int = 50, stride: int = 20, n_positives: int = 30, device=None) -> None:
-        super().__init__(size, stride, device)
-        self.n_positives = n_positives
-
-    def select(self, attended_image) -> Sequence[Collection[Region]]:
-        channel_attentions = [self.f(channel.unsqueeze(0).unsqueeze(0)) for channel in attended_image[0]]  # Assuming batch size of 1
-        channel_regions = [self.transform(attention, torch.where(torch.ones_like(attention))) for attention in channel_attentions]
-        channel_sorted_regions = [list(sorted(regions, key=lambda region: region.attention, reverse=True)) for regions in channel_regions]
-        return [sorted_regions[:self.n_positives] for sorted_regions in channel_sorted_regions]
