e839633d0db7e5935ea36016e68f617ea31a77b3
Nicklas Boserup <nickeopti@gmail.com>
2022-03-26 14:31:39 +0100

    Automate evaluation on test set

--- test_performance.py
+++ test_performance.py
@@ -1,3 +1,4 @@
+import csv
 import sys
 
 import torch
@@ -11,7 +12,18 @@ d = list(dataset)
 images = torch.stack([e[0] for e in d])
 masks = torch.stack([e[1] for e in d])
 
-model: models.Model = models.Model.load_from_checkpoint(sys.argv[2])
+version = sys.argv[2]
+with open(f'logs/default/version_{version}/metrics.csv') as csv_file:
+    reader = csv.DictReader(csv_file)
+    best_epoch = max(reader, key=lambda e: e['best_val_mdc'])
+    epoch = best_epoch['epoch']
+    step = best_epoch['step']
+    n_classes = len([k for k in best_epoch.keys() if k.startswith('mdc_c')])
+    channel = max(range(n_classes), key=lambda i: best_epoch[f'mdc_c{i+1}'])
+
+print(version, epoch, step, channel)
+
+model: models.Model = models.Model.load_from_checkpoint(f'logs/default/version_{version}/checkpoints/epoch={epoch}-step={step}.ckpt')
 
 attention_maps = model.attention_network(images)
 predicted_masks = attention_maps > 0.5
@@ -21,6 +33,6 @@ tp = torch.logical_and(masks, predicted_masks).flatten(start_dim=2).sum(dim=2)
 fp_fn = torch.logical_xor(masks, predicted_masks).flatten(start_dim=2).sum(dim=2)
 
 dice_scores = 2 * tp / (2 * tp + fp_fn)
-dice_score = dice_scores.mean(dim=0)[sys.argv[3]]
+dice_score = dice_scores.mean(dim=0)[channel]
 
-print(dice_score)
+print('DICE score:', dice_score.item())
