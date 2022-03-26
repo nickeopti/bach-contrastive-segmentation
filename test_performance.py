949303edad3c897738a79666bd161398757ef7a7
Nicklas Boserup <nickeopti@gmail.com>
2022-03-26 19:56:15 +0100

    Plot predictions from the best models in each category

--- test_performance.py
+++ test_performance.py
@@ -6,6 +6,7 @@ import torch
 
 import data
 import models
+import plot
 
 
 dataset = data.MoNuSegValidationDataset(sys.argv[1])
@@ -23,8 +24,10 @@ def test_performance(version):
         channel = max(range(n_classes), key=lambda i: best_epoch[f'mdc_c{i+1}'])
 
     model: models.Model = models.Model.load_from_checkpoint(f'logs/default/version_{version}/checkpoints/epoch={epoch}-step={step}.ckpt')
+    model.eval()
 
-    attention_maps = model.attention_network(images)
+    with torch.no_grad():
+        attention_maps = model.attention_network(images)
     predicted_masks = attention_maps > 0.5
 
     tp = torch.logical_and(masks, predicted_masks).flatten(start_dim=2).sum(dim=2)
@@ -33,7 +36,7 @@ def test_performance(version):
     dice_scores = 2 * tp / (2 * tp + fp_fn)
     dice_score = dice_scores.mean(dim=0)[channel].item()
 
-    return dice_score, best_epoch['best_val_mdc']
+    return dice_score, best_epoch['best_val_mdc'], version, epoch, step, channel
 
 
 if __name__ == '__main__':
@@ -45,12 +48,28 @@ if __name__ == '__main__':
     for version, group in info:
         print(version, group)
 
-        test, val = test_performance(version)
-        results[group].append((test, val))
+        r = test_performance(version)
+        results[group].append(r)
 
-        print('DICE score:', test)
+        print('DICE score:', r[0])
         print()
 
-    for group, res in results.items():
-        score = max(res, key=lambda e: e[1])[0]
-        print(group, score)
+    best = {group: max(res, key=lambda e: e[1]) for group, res in results.items()}
+
+    for group, res in best.items():
+        print(group, res[0])
+
+    groups_order, best_values = zip(*best.items())
+    print(groups_order)
+
+    predictions = []
+    for _, _, version, epoch, step, channel in best_values:
+        model = models.Model.load_from_checkpoint(f'logs/default/version_{version}/checkpoints/epoch={epoch}-step={step}.ckpt')
+        model.eval()
+        with torch.no_grad():
+            attention_maps = model.attention_network(images)[:, channel]
+        predictions.append(attention_maps)
+
+    predictions = torch.stack(predictions)
+    predictions = predictions.permute(1, 0, 2, 3)
+    plot.plot_mask(images, masks, predictions, path="test_best_models_predictions.png")
