190a5454761b26a4837519f8005016e92cd07e62
Nicklas Boserup <nickeopti@gmail.com>
2022-05-09 18:03:54 +0200

    Support None as a valid value, even as default value

--- arguments.py
+++ arguments.py
@@ -1,5 +1,5 @@
 import inspect
-from argparse import ArgumentParser
+from argparse import ArgumentParser, _StoreAction
 from dataclasses import dataclass
 from typing import Any, Dict, Sequence, Type, TypeVar
 
@@ -15,14 +15,21 @@ class ClassArguments:
 def add_options(argument_parser: ArgumentParser, name: str, options: Sequence[Type[T]]) -> ClassArguments:
     argument_group = argument_parser.add_argument_group(name)
 
-    argument_group.add_argument(f"--{name}", type=str, default=options[0].__name__)
+    argument_group.add_argument(f"--{name}", type=str, default=options[0].__name__ if options[0] is not None else None)
     temp_args, _ = argument_parser.parse_known_args()
 
     selected_class_name = vars(temp_args)[name]
-    selected_class = {c.__name__: c for c in options}[selected_class_name]
+    if selected_class_name is None:
+        # No value was given, and the default is None
+        return None
 
+    selected_class = {c.__name__: c for c in options if c is not None}[selected_class_name]
+
+    previously_known_arguments = [argument.option_strings[0][2:] for argument in argument_parser._actions if isinstance(argument, _StoreAction)]
     arguments = _get_arguments(selected_class)
     for argument in arguments:
+        if argument.name in previously_known_arguments:
+            continue
         argument_group.add_argument(
             f"--{argument.name}",
             type=argument.annotation,
